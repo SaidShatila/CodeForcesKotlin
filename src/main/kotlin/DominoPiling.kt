a364891443684fd61a4ba3ff12cb512e1c252901
SaidShatilaToters <said.shatila@totersapp.com>
2023-05-24 23:26:01 +0300

    finished dominoPilling

--- src/main/kotlin/DominoPiling.kt
+++ src/main/kotlin/DominoPiling.kt
@@ -1,8 +1,10 @@
-fun main(){
+import java.util.*
 
+fun main() {
+    val sc = Scanner(System.`in`)
+    val m = sc.nextInt()
+    val n = sc.nextInt()
+    println(dominoPiling(m, n))
 }
 
-
-fun dominoPiling(){
-
-}
+fun dominoPiling(m: Int, n: Int): Int = (m * n).floorDiv(2)
